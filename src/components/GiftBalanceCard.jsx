ef3d5b176d85d664255ce593f9bc4c907e35eba1
Pete Gregory <boris.the.alien@gmail.com>
2022-04-08 12:42:32 +0100

    gift card amount shown

--- src/components/GiftBalanceCard.jsx
+++ src/components/GiftBalanceCard.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import BalanceIcon from "../assets/balance.png";
 const GiftBalanceCard = () => {
+    const [balance, setBalance] = useState(0.0);
+
+    const formatter = new Intl.NumberFormat("en-GB", {
+        style: "currency",
+        currency: "GBP",
+        minimumFractionDigits: 2,
+    });
+
+    const checkCard = (e) => {
+        e.preventDefault();
+        if (document.getElementById("balance-code").value !== "") {
+            setBalance(getRandomNumber(1, 100));
+        }
+    };
+
+    function getRandomNumber(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
     return (
         <div className="card balance">
             <div className="balance-header">
                 <img className="balance-icon" alt="balance" src={BalanceIcon} />
                 <label className="balance-title">Gift Card Balance</label>
-                <label className="balance-amount"> - 0.00 GBP</label>
+                <label className="balance-amount">
+                    {" - " + formatter.format(balance)}
+                </label>
             </div>
-            <form className="balance-form">
+            <form className="balance-form" onSubmit={checkCard}>
                 <input
+                    id="balance-code"
                     className="balance-code"
                     type="text"
                     placeholder="ENTER CODE"
